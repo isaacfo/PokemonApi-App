dfa4d08538cd057720a8b03294ded88d44b3512d
Isaac Fonseca <isaacfo1010@gmail.com>
2018-10-17 15:25:26 -0400

    added functions for caching last pokemon called

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,24 @@
-// // DOM selection
-// const triggerElement = document.querySelector('[ data-trigger]');
-// const outputElement = document.querySelector('[ data-output]');
 
-// // function that gets a chuck norris joke
-// function getTypes() {
-//     fetch('https://swapi.co/api/people/1/')
-//         .then(convertToJson)
-//         .then(extractTypesText)
-//         .then(drawTypes)
-// }
-
-// function convertToJson(r) {
-//     r = r.json()
-//     return r;
-// }
-
-// function extractTypesText(nameObj) {
-//     return [nameObj.name, nameObj.eye_color];
-// }
-
-// // function that draws joke to DOM
-// function drawTypes(TypeText) {
-//     const newType = document.createElement('li');
-//     newType.textContent = TypeText;
-
-//     // not returning, just appending
-//     outputElement.appendChild(newType);
-// }
-
-
-// // main function that attaches button listener
-// function main() {
-//     triggerElement.addEventListener('click', getTypes);
-// }
-// main();
 
 
 // DOM selection
 const triggerElement = document.querySelector('[ data-trigger]');
 const outputElement = document.querySelector('[ data-output]');
 
-// function that gets a chuck norris joke
+
+// Global pokemon cache
+let cachedPokemon = 'charmander';
+let pokemonPromise;
+
+
+// function that gets a pokemon based on thier pokedex #
 function getPokemon() {
     let pokemonNum = randomPokemon();
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNum}/`)
         .then(convertToJson)
+        .then(cachePokemon)
         .then(extractPokemonText)
+        .catch(showCachedPokemon)
         .then(drawPokemon)
 }
 // function that creates random number for Api fucntion to fetch
@@ -58,10 +31,47 @@ function convertToJson(r) {
     r = r.json()
     return r;
 }
-//  returns pokemon name and type 
-function extractPokemonText(nameObj) {
-    return [nameObj.name, nameObj.types[0].type.name];
+
+
+function cachePokemon(pokemonObj) {
+    // save pokemonJson
+    // 1. global variable
+    // cachedPokemon = pokemonObj.name || cachedPokemon; // keep what's in cachedPokemon if pokemonObj.name is falsey
+    // return pokemonObj;
+    // 2. localStorage.
+    // Letting JS figure out if .pokemon exists.
+    // That is - do we have a string, or do we have undefined?
+    if (pokemonObj.name) { //opting in to "coercion"
+        console.log('Caching pokemon to localStorage');
+        localStorage.setItem('pokemon', JSON.stringify(pokemonObj));
+    }
+    return pokemonObj;
 }
+
+
+
+
+
+
+function showCachedPokemon(err) {
+    console.log(err);
+     // drawPokemon(cachedPokemon);
+    return JSON.parse(localStorage.getItem('pokemon'));
+}
+
+
+
+//  returns pokemon name and type 
+function extractPokemonText(pokemonObj) {
+    //debugger;
+    //console.log(pokemonObj);
+    return [pokemonObj.name, pokemonObj.types[0].type.name];
+    //return 'charmander'
+}
+
+
+
+
 
 // function that draws joke to DOM
 function drawPokemon(pokemonText) {
